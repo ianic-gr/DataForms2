88095fa19b1d8e1f0f5759527359ed47855cbae8
ianictachos <tachos@ianic.gr>
2024-10-09 14:45:08 +0300

    Updates

--- assets/epic-tezOSbDE.js
+++
@@ -1,9 +0,0 @@
-import{a as s,b as a,aR as r}from"./index-CSu0vrla.js";const n={__name:"epic",setup(o){const e={rows:[{title:"Epic with Text",input:[{epic:{type:"epic",options:{label:"Pop",text:`
-                    <h1>Lorem ipsum dolor sit amet,</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum pulvinar nibh dignissim dignissim. Proin eget sem tempus, efficitur sapien non, iaculis mi. Nullam id ipsum tortor. Mauris porta sem vel lorem ultricies suscipit.</p>
-                    <p>Maecenas sed ante id tortor varius tempus. <b>Donec non elit at ipsum lacinia faucibus.</b> Fusce consequat ut quam ullamcorper ornare. Curabitur suscipit blandit nulla eu pretium.</p>
-                    `}}}]},{title:"Epic with HTML",input:[{epic:{type:"epic",options:{label:"Pop",html:`
-                    <h1>Lorem ipsum dolor sit amet,</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum pulvinar nibh dignissim dignissim. Proin eget sem tempus, efficitur sapien non, iaculis mi. Nullam id ipsum tortor. Mauris porta sem vel lorem ultricies suscipit.</p>
-                    <p>Maecenas sed ante id tortor varius tempus. <b>Donec non elit at ipsum lacinia faucibus.</b> Fusce consequat ut quam ullamcorper ornare. Curabitur suscipit blandit nulla eu pretium.</p>
-                    `}}}]}],submit:{title:"Send Message",color:"primary",click(i){console.log("data: ",i)}}};return(i,u)=>{const t=r("DataForm");return s(),a(t,{id:"epic",api:e})}}};export{n as default};
