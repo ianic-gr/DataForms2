cb61aa0859c35a0440894ba464c31c1dc03f045e
ianictachos <tachos@ianic.gr>
2024-10-31 12:32:24 +0200

    Updates

---
+++ assets/epic-Bp-cnnEd.js
@@ -0,0 +1,9 @@
+import{b as s,aR as a,a as r}from"./index-C5-7oPDj.js";const n={__name:"epic",setup(o){const e={rows:[{title:"Epic with Text",input:[{epic:{type:"epic",options:{label:"Pop",text:`
+                    <h1>Lorem ipsum dolor sit amet,</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum pulvinar nibh dignissim dignissim. Proin eget sem tempus, efficitur sapien non, iaculis mi. Nullam id ipsum tortor. Mauris porta sem vel lorem ultricies suscipit.</p>
+                    <p>Maecenas sed ante id tortor varius tempus. <b>Donec non elit at ipsum lacinia faucibus.</b> Fusce consequat ut quam ullamcorper ornare. Curabitur suscipit blandit nulla eu pretium.</p>
+                    `}}}]},{title:"Epic with HTML",input:[{epic:{type:"epic",options:{label:"Pop",html:`
+                    <h1>Lorem ipsum dolor sit amet,</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rutrum pulvinar nibh dignissim dignissim. Proin eget sem tempus, efficitur sapien non, iaculis mi. Nullam id ipsum tortor. Mauris porta sem vel lorem ultricies suscipit.</p>
+                    <p>Maecenas sed ante id tortor varius tempus. <b>Donec non elit at ipsum lacinia faucibus.</b> Fusce consequat ut quam ullamcorper ornare. Curabitur suscipit blandit nulla eu pretium.</p>
+                    `}}}]}],submit:{title:"Send Message",color:"primary",click(i){console.log("data: ",i)}}};return(i,u)=>{const t=a("DataForm");return r(),s(t,{id:"epic",api:e})}}};export{n as default};
---
+++ assets/index-CGl18y__.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index-C5-7oPDj.js";const e={};function o(r,c){return"Coming Soon..."}const _=n(e,[["render",o]]);export{_ as default};
